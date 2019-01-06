383e98089e0192bca31ad53bd986201822f02331
javcof <javcof@qq.com>
2019-01-06 23:45:49 +0800

    add example other comp

---
+++ example/src/components/other/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { defineMessages, FormattedMessage } from 'react-intl'
+
+const Lang = defineMessages({
+  hello: 'other hello',
+  world: 'other world',
+})
+
+export default class Other extends React.Component {
+  render() {
+    return (
+        <div>
+          <FormattedMessage {...Lang.hello} />
+          <br />
+          <FormattedMessage {...Lang.world} />
+        </div>
+    )
+  }
+}
